b5414fcb3053f293dedb39e569e7ba75fe808fb0
jsobrinosolis <j.sobrino3@usp.ceu.es>
2022-04-26 18:16:24 +0200

    /lugares con objeto mock hecha

---
+++ routes/lugares.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('lugares', { title: 'Lugares', array_lugares})
+});
+
+router.post('/', function(req, res, next) {
+  array_lugares.push({
+    nombre: req.body.nombre,
+    ubicacion: req.body.ubicacion,
+    ciudad: req.body.ciudad
+  })
+  res.render('lugares', { title: 'Lugares', array_lugares})
+});
+
+array_lugares = [
+  {
+    nombre: "Santiago Bernabéu",
+    ubicacion: "Paseo de la Castellana nº 138",
+    ciudad: "Madrid"
+  },
+  {
+    nombre: "Universidad CEU San Pablo",
+    ubicacion: "Calle Julián Romea nº 23",
+    ciudad: "Madrid"
+  }
+]
+
+module.exports = router;
