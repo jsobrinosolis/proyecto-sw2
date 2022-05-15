b2a821a814241f0e4d33bdf51b6d5d966172a8e6
jsobrinosolis <j.sobrino3@usp.ceu.es>
2022-05-15 22:32:35 +0200

    /organizadores done

--- routes/organizadores.js
+++ routes/organizadores.js
@@ -1,35 +1,87 @@
 var express = require('express');
 var router = express.Router();
+const dbo = require('../db/connection');
+const {ObjectID} = require("mongodb");
 
-/* GET home page. */
+/* GET all organizadores. */
 router.get('/', function(req, res, next) {
-  res.json(array_organizadores)
+  const connection = dbo.getDb();
+  connection.collection('organizadores').find({}).limit(5)
+      .toArray(function (err, result){
+        if (err){
+          res.status(400).send('Error al buscar organizadores');
+        } else {
+          res.json(result);
+        }
+      });
 });
 
-router.post('/', function(req, res, next) {
-  array_organizadores.push({
+/* Crear organizador */
+router.post('/', async function(req, res, next) {
+  const connection = dbo.getDb();
+  const organizador = {
     nombre_empresa: req.body.nombre_empresa,
     pais_origen: req.body.pais_origen,
     owner: req.body.owner
-  })
-  res.json(array_organizadores)
-});
-array_organizadores = [
-  { nombre_empresa: "De la Riva",
-    pais_origen: "España",
-    owner: "José Morán Arteaga"
-  },
-  { nombre_empresa: "Ejemplo",
-    pais_origen: "EEUU",
-    owners: [
-      {
-        nombre: "Elon Musk"
-      },
-      {
-        nombre: "Jeff Bezos"
-      }
-    ]
   }
-]
+  connection
+      .collection('organizadores')
+      .insertOne(organizador, function (err, result){
+        if (err){
+          res.status(400).send('Error al crear el organizador');
+        } else {
+          res.status(201).send();
+        }
+      });
+})
+
+
+/* GET organizador by ID. */
+router.get('/:id', async function(req, res, next) {
+  const connection = dbo.getDb();
+  connection
+      .collection('organizadores')
+      .findOne({_id:ObjectID(req.params.id)}, function(err, result){
+        if (err){
+          res.status(400).send('Error al acceder al organizador');
+        } else {
+          res.json(result);
+        }
+      });
+});
+
+
+/* PUT organizador by ID. */
+router.put('/:id', async function(req, res, next) {
+  const connection = dbo.getDb();
+  connection
+      .collection('organizadores')
+      .updateOne({_id:ObjectID(req.params.id)}, {$set:{
+          nombre_empresa: req.body.nombre_empresa,
+          pais_origen: req.body.pais_origen,
+          owner: req.body.owner
+        }}, function(err, result){
+        if (err){
+          res.status(400).send('Error al actualizar la información de un organizador');
+        } else {
+          res.status(204).send();
+        }
+      });
+});
+
+
+/* PUT organizador by ID. */
+router.delete('/:id', async function(req, res, next) {
+  const connection = dbo.getDb();
+  connection
+      .collection('organizadores')
+      .deleteOne({_id:ObjectID(req.params.id)}, function(err, result){
+        if (err){
+          res.status(400).send('Error al borrar un organizador');
+        } else {
+          res.status(200).send();
+        }
+      });
+});
 
 module.exports = router;
