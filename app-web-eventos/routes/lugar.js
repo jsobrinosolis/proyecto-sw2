23ffe00f5885f91b802229082ac5ef088ce462d9
jsobrinosolis <j.sobrino3@usp.ceu.es>
2022-06-12 18:12:09 +0200

    Tablas GET by ID

--- app-web-eventos/routes/lugar.js
+++ app-web-eventos/routes/lugar.js
@@ -3,15 +3,15 @@ var router = express.Router();
 const axios = require('axios');
 
 router.get('/', function(req, res, next) {
-    res.render('lugar', { title: 'Lugar by ID' });
+    res.render('lugar', { title: 'Lugar by ID', location: {} });
 });
 
 router.post('/', function(req, res, next) {
     axios.get('http://localhost:3000/lugares/' + req.body.id)
         .then(response => {
             console.log(response.data);
-            //res.render('lugares', { title: 'Lugares', locations: response.data });
-            res.json(response.data);
+            res.render('lugar', { title: 'Lugar by ID', location: response.data });
+            //res.json(response.data);
         })
         .catch(error => {
             console.log(error);
--- app-web-eventos/routes/organizador.js
+++ app-web-eventos/routes/organizador.js
@@ -3,15 +3,15 @@ var router = express.Router();
 const axios = require('axios');
 
 router.get('/', function(req, res, next) {
-    res.render('organizador', { title: 'Organizador by ID' });
+    res.render('organizador', { title: 'Organizador by ID', organizador: {} });
 });
 
 router.post('/', function(req, res, next) {
     axios.get('http://localhost:3000/organizadores/' + req.body.id)
         .then(response => {
             console.log(response.data);
-            //res.render('lugares', { title: 'Lugares', locations: response.data });
-            res.json(response.data);
+            res.render('organizador', { title: 'Organizador by ID', organizador: response.data });
+            //res.json(response.data);
         })
         .catch(error => {
             console.log(error);
